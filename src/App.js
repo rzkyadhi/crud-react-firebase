8172fb0eb86588a5d52ff454aa1add9174f472a3
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-01-05 18:00:11 +0700

    Read the Data from Firestore

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import { db } from './config/firebase'
+import { collection, doc, getDocs } from 'firebase/firestore'
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+  useEffect(() => {
+    const getUsers = async() => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    }
+
+    getUsers()
+  }, [])
   return (
-    <div className="App"></div>
+    <div className="App"> 
+      { users.map((user) => {
+        return (
+          <div>
+            {" "}
+            <h1>Name: { user.name }</h1>
+            <h1>Age: { user.age }</h1>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBIdGCmRZTPN05qdeAxAHT73uR2JFF6n3c",
+    authDomain: "reactfirebase-db572.firebaseapp.com",
+    projectId: "reactfirebase-db572",
+    storageBucket: "reactfirebase-db572.appspot.com",
+    messagingSenderId: "500208914560",
+    appId: "1:500208914560:web:a32dda9b87a976b4915665",
+    measurementId: "G-WLSW8VTBG5"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  export const db = getFirestore(app);
