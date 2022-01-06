81df53e76bd5de868cf30d4e4a0a4c1370e0faf7
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-01-06 17:05:45 +0700

    Finishing

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBIdGCmRZTPN05qdeAxAHT73uR2JFF6n3c",
-    authDomain: "reactfirebase-db572.firebaseapp.com",
-    projectId: "reactfirebase-db572",
-    storageBucket: "reactfirebase-db572.appspot.com",
-    messagingSenderId: "500208914560",
-    appId: "1:500208914560:web:a32dda9b87a976b4915665"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   const app = initializeApp(firebaseConfig);
